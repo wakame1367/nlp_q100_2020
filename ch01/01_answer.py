a90150e4643411831c2fd225af2d77d8b4c7b142
wakamezake <hotwater1367@gmail.com>
2020-04-12 13:19:58 +0900

    init

---
+++ ch01/01_answer.py
@@ -0,0 +1,7 @@
+"""
+https://nlp100.github.io/ja/ch01.html#01-%E3%83%91%E3%82%BF%E3%83%88%E3%82%AF%E3%82%AB%E3%82%B7%E3%83%BC%E3%83%BC
+"""
+
+if __name__ == '__main__':
+    q = "パタトクカシーー"
+    print(q[1::2])
