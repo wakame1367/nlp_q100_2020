7d89e182ec12c0c77d4a78d32f07088b78608855
wakamezake <hotwater1367@gmail.com>
2020-04-12 14:22:24 +0900

    typo

--- ch01/04_answer.py
+++ ch01/04_answer.py
@@ -1,7 +1,6 @@
 """
 https://nlp100.github.io/ja/ch01.html#04-%E5%85%83%E7%B4%A0%E8%A8%98%E5%8F%B7
 """
-import re
 
 if __name__ == '__main__':
     q = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations" \
