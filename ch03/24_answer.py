8cc773cc2530116481102330841c19322b807cdb
wakamezake <hotwater1367@gmail.com>
2020-04-13 12:31:18 +0900

    init

--- ch03/24_answer.py
+++ ch03/24_answer.py
@@ -1,5 +1,5 @@
 """
-https://nlp100.github.io/ja/ch03.html#23-%E3%82%BB%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3%E6%A7%8B%E9%80%A0
+https://nlp100.github.io/ja/ch03.html#24-%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E5%8F%82%E7%85%A7%E3%81%AE%E6%8A%BD%E5%87%BA
 """
 import re
 from util import get_text
