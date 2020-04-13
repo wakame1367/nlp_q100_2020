701874c251192020b30e5ef8ac444c1d09d0a83f
wakamezake <hotwater1367@gmail.com>
2020-04-13 12:20:35 +0900

    init

---
+++ ch03/22_answer.py
@@ -0,0 +1,11 @@
+"""
+https://nlp100.github.io/ja/ch03.html#22-%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E5%90%8D%E3%81%AE%E6%8A%BD%E5%87%BA
+"""
+import re
+from util import get_text
+
+if __name__ == '__main__':
+    pat = re.compile(r'\[\[Category:(.*?)(?:\|.*)?\]\]')
+    text = get_text()
+    for match in re.finditer(pat, text):
+        print(match.group(1))
