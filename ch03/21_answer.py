01d0cbeca67c188aa02023049f342b44af1418e8
wakamezake <hotwater1367@gmail.com>
2020-04-13 12:07:38 +0900

    typo

--- ch03/21_answer.py
+++ ch03/21_answer.py
@@ -1,5 +1,5 @@
 """
-https://nlp100.github.io/ja/ch01.html#00-%E6%96%87%E5%AD%97%E5%88%97%E3%81%AE%E9%80%86%E9%A0%86
+https://nlp100.github.io/ja/ch03.html#21-%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E5%90%8D%E3%82%92%E5%90%AB%E3%82%80%E8%A1%8C%E3%82%92%E6%8A%BD%E5%87%BA
 """
 import re
 from util import get_text
