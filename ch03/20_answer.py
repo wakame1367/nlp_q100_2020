46116551f8e384e9aa6c6d66d2041002b9840908
wakamezake <hotwater1367@gmail.com>
2020-04-13 11:55:11 +0900

    upd8

--- ch03/20_answer.py
+++ ch03/20_answer.py
@@ -1,9 +1,7 @@
 """
 https://nlp100.github.io/ja/ch01.html#00-%E6%96%87%E5%AD%97%E5%88%97%E3%81%AE%E9%80%86%E9%A0%86
 """
-from util import load_json
+from util import get_text
 
 if __name__ == '__main__':
-    wiki_json = load_json()
-    query = "イギリス"
-    print(wiki_json[wiki_json["title"] == query]["text"].values[0])
+    print(get_text())
--- ch03/util.py
+++ ch03/util.py
@@ -10,3 +10,9 @@ JSON_PATH = INPUT_PATH / "jawiki-country.json.gz"
 
 def load_json():
     return pd.read_json(JSON_PATH, lines=True, compression='infer')
+
+
+def get_text():
+    wiki_json = load_json()
+    query = "イギリス"
+    return wiki_json[wiki_json["title"] == query]["text"].values[0]
