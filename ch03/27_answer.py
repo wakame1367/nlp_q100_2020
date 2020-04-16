088e164b859291babd3aa61278840d46a60a35d3
wakamezake <hotwater1367@gmail.com>
2020-04-16 14:22:48 +0900

    init

---
+++ ch03/27_answer.py
@@ -0,0 +1,28 @@
+"""
+https://nlp100.github.io/ja/ch03.html#25-%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%81%AE%E6%8A%BD%E5%87%BA
+"""
+import re
+from util import get_text
+from pprint import pprint
+
+if __name__ == '__main__':
+    pat = re.compile(r'^\{\{基礎情報.*?$(.*?)^\}\}',
+                     re.MULTILINE + re.DOTALL)
+    text = get_text()
+    match = pat.findall(text)
+    match_text = match[0]
+    sep = ' = '
+    file_markup = 'ファイル:'
+    result = {}
+    markup_pat = re.compile(r'\'{2,5}')
+    inline_link_pat = re.compile(r'\[\[(?!ファイル:)(.*?)\]\]')
+    for t in match_text.split('\n'):
+        if sep in t:
+            split_text = t.split(' = ')
+            key = split_text[0].lstrip('|')
+            value = split_text[1]
+            remove_text = markup_pat.sub('', value)
+            remove_text = inline_link_pat.sub(r'\1', remove_text)
+            result[key] = remove_text
+    pprint(result)
+
